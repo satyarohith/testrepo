c6eb1c02961c026f69c200b300ab001d781166d5
beyou1 <97203423+beyou1@users.noreply.github.com>
2022-01-06 13:22:08 +0530

    the explorer

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 function handler(req: Request): Response {
   console.log(req);
-  return new Response("I'm beyou1");
+  return new Response("I'm beyou1 - the explorer!");
 }
 
 console.log("Listening on http://localhost:8000");
