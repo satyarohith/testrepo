1e4abcc5143c47c794f205df82db5f5ec39874ab
Satya Rohith <me@satyarohith.com>
2022-01-06 18:10:34 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 function handler(req: Request): Response {
-  return new Response("Hello World!");
+  return new Response("Hello World! Stay Happy!");
 }
 
 console.log("Listening on http://localhost:8000");
