44639a65c2b11c651e5f4c784f9e131ea2974e0f
beyou1 <97203423+beyou1@users.noreply.github.com>
2022-01-06 15:26:04 +0530

    staging fix

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 function handler(req: Request): Response {
   console.log(req);
-  return new Response("I'm beyou1 - the explorer, on staging!");
+  return new Response("I'm beyou1 - the explorer, on staging! ");
 }
 
 console.log("Listening on http://localhost:8000");
