2eb3a539aebf0d3b728c5a89daa4c00c071eb69e
Satya Rohith <me@satyarohith.com>
2022-01-07 20:09:44 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 function handler(req: Request): Response {
-  return new Response("Hello from GitHub");
+  return new Response("Hello World!");
 }
 
 console.log("Listening on http://localhost:8000");
