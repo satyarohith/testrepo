c87e99a3f41beb390f3a7cc1b654cdddc5427827
Satya Rohith <me@satyarohith.com>
2022-02-08 13:49:09 +0530

    return hello

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
 function handler(req: Request): Promise<Response> {
-  const url = new URL("./main.ts", import.meta.url);
-  return fetch(url);
+  
+  return new Response("hello")
 }
 
 console.log("Listening on http://localhost:8000");
