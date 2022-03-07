5d30c7976b388ea0212f3f17e67e72e1f0744e19
Satya Rohith <me@satyarohith.com>
2022-03-07 21:04:00 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,6 @@
-import { Application } from 'https://deno.land/x/oak@v10.4.0/mod.ts'
+import { serve, serveStatic } from "https://deno.land/x/sift@0.4.3/mod.ts";
 
-const port = parseInt(Deno.env.get('PORT') || '4008', 10)
-const app = new Application()
-
-app.use(async ctx => {
-  await ctx.send({
-    root: Deno.cwd(),
-    index: 'readme.txt'
-  })
-})
-
-console.debug(`Listening on 0.0.0.0:${port}`)
-await app.listen({ port })
+serve({
+  // You can serve a single file.
+  "/": serveStatic("readme.txt", { baseUrl: import.meta.url }),
+});
